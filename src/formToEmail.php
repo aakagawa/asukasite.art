299d947407ef447fbf0c19eb857da44f54291aa3
Asuka Akagawa <aakagawa@berklee.edu>
2021-05-28 14:33:38 -0400

    creation of email submission form and .php

---
+++ src/formToEmail.php
@@ -0,0 +1,26 @@
+<?php
+    if(!isset($_POST['submit']))
+    {
+        echo "error; please submit the form!";
+    }
+
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['inquiry'];
+
+    if(empty($name)||empty($visitor_email||empty($message))) {
+        echo "Error: all fields are required!";
+        exit;
+    }
+
+    $email_from = 'info@asukasite.art';
+    $email_subject = 'New inquiry';
+    $email_body = '$message';
+
+    $to = 'info@asukasite.art';
+    $headers = 'from: $email_from';
+    $headers .= "Reply-To: $visitor_email";
+
+    mail($to,$email_subject,$email_body,$headers);
+?>
+
