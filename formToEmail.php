a12f2e7d3b9711987e39b33185fe8844540b5dfb
Asuka Akagawa <aakagawa@berklee.edu>
2021-06-09 18:35:45 -0400

    Modified formToEmail.php

--- formToEmail.php
+++ formToEmail.php
@@ -4,9 +4,13 @@
     $message = $_POST['message'];
   
     $to = 'info@asukasite.art';
-    $headers = 'From: '.$name.'<'.$visitor_email.'>'; 
+
     $email_subject = "New inquiry!";
     $email_body = "$message";
+
+    $headers = "MIME-Version: 1.0" . "\n";
+    $headers .= "Content-type:text/html;charset=iso-8859-1" . "\n";
+    $headers .= "From: $visitor_email" . "\n";
   
 
     mail($to,$email_subject,$email_body,$headers);
