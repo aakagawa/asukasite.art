7ba4a8f3fb624085e3bb97161348d41020338d44
Asuka Akagawa <aakagawa@berklee.edu>
2021-06-09 16:57:31 -0400

    Modified formToEmail.php location

--- formToEmail.php
+++ formToEmail.php
@@ -1,7 +1,35 @@
 <?php
+    function IsInjected($str) {
+        $injections = array('(\n+)',
+               '(\r+)',
+               '(\t+)',
+               '(%0A+)',
+               '(%0D+)',
+               '(%08+)',
+               '(%09+)'
+               );
+                   
+        $inject = join('|', $injections);
+        $inject = "/$inject/i";
+        
+        if(preg_match($inject,$str))
+        {
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+    }
+    
+    if(IsInjected($visitor_email)){
+    echo "Bad email value!";
+    exit;
+    }
+
     $name = $_POST['name'];
     $visitor_email = $_POST['email'];
-    $message = $_POST['inquiry'];
+    $message = $_POST['message'];
 
     if(empty($name)||empty($visitor_email)||empty($message)) {
         echo "Error: all fields are required!";
