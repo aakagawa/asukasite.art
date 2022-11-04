0dfb1216097e20c3c6ec2d7bf843300fda371dbf
Asuka Akagawa <aakagawa@berklee.edu>
2022-11-04 18:37:19 +0100

    Attempt at including compression.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const compression = require('compression');
 const express = require('express');
 
 const { resolve } = require("path");
 const app = express();
 const port = process.env.PORT || 8000;
 const http = require('http');
+
+app.use(compression());
 
 app.set('port', port);
 app.use(express.static(__dirname + '/src'));
@@ -14,3 +17,4 @@ app.get('*', (req, res)  => {
 
 
 http.createServer(app).listen(port);
+
