ba353918ba1ee37756d49e8f948b8b3e3d979115
Asuka Akagawa <aakagawa@berklee.edu>
2022-11-14 09:39:22 +0100

    Updated script for image sizing.

--- src/scripts/pictureFullScreen.js
+++ src/scripts/pictureFullScreen.js
@@ -1,9 +1,4 @@
 var pictures = {
-    Stairs_Installation: [
-        "",
-        "",
-        "",
-    ],
     IMG_0584: [
         "https://cloud-cube-us2.s3.amazonaws.com/n76qf8iabaqh/public/visuals/photos/IMG_0584.jpg"
     ],
