b10ddd9ac650c4751d8edc4d8fc7f95d8883b30e
aakagawa <asukaakagawa.art@gmail.com>
2023-06-30 18:15:47 +0200

    Fixed design issue.

--- src/scripts/pictureFullScreen.js
+++ src/scripts/pictureFullScreen.js
@@ -1,6 +1,6 @@
 var pictures = {
     IMG_0584: [
-        "https://cloud-cube-us2.s3.amazonaws.com/n76qf8iabaqh/public/visuals/photos/IMG_0584.jpg"
+        "https://cloud-cube-us2.s3.amazonaws.com/n76qf8iabaqh/public/visuals/photos/IMG_0584/IMG_0584.jpg"
     ],
     Spring:
     [
