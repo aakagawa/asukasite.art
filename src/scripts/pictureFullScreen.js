cba7b6296614826227764fa65224e7ff687c8ed2
aakagawa <60159988+aakagawa@users.noreply.github.com>
2023-12-25 16:59:52 +0900

    Major update on design. Canceled icons for navicons and introduced selectable text. Fixed minor bugs on soundsPage.

--- src/scripts/pictureFullScreen.js
+++ src/scripts/pictureFullScreen.js
@@ -2,6 +2,9 @@ var pictures = {
     IMG_0584: [
         "https://cloud-cube-us2.s3.amazonaws.com/n76qf8iabaqh/public/visuals/photos/IMG_0584/IMG_0584.jpg"
     ],
+    for_kensuke: [
+        "https://cloud-cube-us2.s3.amazonaws.com/n76qf8iabaqh/public/visuals/installation/230330_for_kensuke.png"
+    ]
 }
 
 var imgThumbnail = document.getElementsByClassName('imgThumbnail');
@@ -15,7 +18,7 @@ for (var o = 0; o < imgThumbnail.length; o++) {
 }
 
 function pictureFullScreen() {
-    thisPicture = this.parentElement.children[1].id;
+    thisPicture = this.parentElement.parentElement.parentElement.children[1].id;
     fullScreenPicture.src = pictures[thisPicture][0];
     preloader.style.display = "block";
     loader.style.display = "block";
@@ -23,23 +26,23 @@ function pictureFullScreen() {
 }
 
 function pictureLoaded() { 
-    fullScreenPicture.addEventListener('load', function() { 
+    fullScreenPicture.addEventListener('load', () => { 
         fullScreenBackground.style.display = "block"
         fullScreenPicture.style.display = "block";
         exitFullScreen.style.display = "block";
         preloader.style.display = "none";
         loader.style.display = "none";
-        setTimeout(function() {
+        setTimeout(() => {
             exitFullScreen.style.display = "none";
         }, 2000);
         fullScreenPicture.addEventListener('mousemove', pictureFullScreenIO);
-        exitFullScreen.addEventListener('click', function() {
+        exitFullScreen.addEventListener('click', () => {
             console.log('exitfullscreen clicked picture');
             fullScreenBackground.style.display = "none"; //black background
             fullScreenPicture.style.display = "none"; //the picture 
             exitFullScreen.style.display = "none";
         });
-        document.addEventListener('keydown', function(event) {
+        document.addEventListener('keydown', (event) => {
             if (event.key === "Escape") {
                 fullScreenBackground.style.display = "none"; //black background
                 fullScreenPicture.style.display = "none"
@@ -52,7 +55,7 @@ function pictureLoaded() {
 function pictureFullScreenIO() {
     exitFullScreen.style.display = "block";
     fullScreenPicture.removeEventListener('mousemove', pictureFullScreenIO);
-    setTimeout(function () {
+    setTimeout(() => {
         exitFullScreen.style.display = "none";
         fullScreenPicture.addEventListener('mousemove', pictureFullScreenIO); 
     }, 2000);
